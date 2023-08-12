6ff46958fb21153fd585bdc3c66ac31c68b3cf26
Majhrs16 <majhrs16.7@gmail.com>
2023-08-12 14:15:57 -0400

    Soporte para compilacion.

---
+++ extract_libs.bat
@@ -0,0 +1,15 @@
+cd bin
+jar xf ..\libs\org.json*.jar  || Goto Error
+jar xf ..\libs\mysql*.jar  || Goto Error
+rmdir /S /Q com\mysql\cj || Goto Error
+rmdir /S /Q META-INF || Goto Error
+del INFO_BIN
+del INFO_SRC
+del LICENSE || Goto Error
+del README
+del VERSION
+cd ..
+Exit /B 0
+
+:Error
+	Exit /B %ERRORLEVEL%
