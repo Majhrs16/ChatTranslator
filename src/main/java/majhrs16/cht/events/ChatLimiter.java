70526fb741fd9c2818f318195f6d6784f64af08d
Majhrs16 <majhrs16.7@gmail.com>
2024-03-09 04:07:54 -0400

    Fixed unnecessary kicks for ChatLimited.run;

--- src/main/java/majhrs16/cht/events/ChatLimiter.java
+++ src/main/java/majhrs16/cht/events/ChatLimiter.java
@@ -1,20 +1,20 @@
 package majhrs16.cht.events;
 
-import majhrs16.lib.logger.Logger;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.scheduler.BukkitTask;
 import org.bukkit.entity.Player;
 import org.bukkit.Bukkit;
 
+import majhrs16.cht.util.cache.SpamTracker;
 import majhrs16.cht.events.custom.Message;
-import majhrs16.cht.util.cache.CacheSpam;
-import majhrs16.cht.util.cache.Config;
 import majhrs16.cht.ChatTranslator;
+
+import majhrs16.lib.logger.Logger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.UUID;
+import java.util.Map;
 
 public class ChatLimiter {
 	private BukkitTask task;
@@ -25,81 +25,73 @@ public class ChatLimiter {
 
 	public void start() {
 		task = Bukkit.getScheduler().runTaskTimer(plugin, new Runnable() {
-			final int max_messages_per_tick     = 7;
-			final Map<Player, CacheSpam> counts = new HashMap<>();
-			FileConfiguration config      = plugin.config.get();
-			CacheSpam spam                = new CacheSpam(0.0D,
-				config.contains("max-spam-per-tick")
-					? config.getDouble("max-spam-per-tick")
-					: 0.0D
-			);
+			final Map<Player, SpamTracker> counts = new HashMap<>();
+			SpamTracker spam = getGlobalSpamTracker();
 
-			public void run() {
-				if (plugin.isDisabled())
-					return;
-
-				if (spam.getCountInt() > spam.getMaxInt() && spam.getMax() > 0.0F) {
-					counts.clear();
-
-					config = plugin.config.get();
-
-					logger.debug("spam.count: %s, spam.max: %s", spam.getCount(), spam.getMax());
-
-					spam = new CacheSpam(0.0D,
-						config.contains("max-spam-per-tick")
-							? config.getDouble("max-spam-per-tick")
-							: 0.0D
-					);
-				}
-				
-				if (!chat.isEmpty()) {
-					int end = Math.min(max_messages_per_tick, chat.size());
-
-					for (Message event : chat.subList(0, end)) {
-						if (event.getSender() instanceof Player) {
-							Player player = (Player) event.getSender();
-
-							if (spam.getMax() > 0.0F) {
-								if (counts.containsKey(player)) {
-									CacheSpam count = counts.get(player);
-										count.setCount(count.getCount() + 0.0001F);
-									counts.put(player, count);
-
-									logger.debug("count: %s, spam.count: %s, spam.max: %s", count.getCount(), spam.getCount(), spam.getMax());
-
-									if (count.getCountDouble() >= spam.getMaxDouble()) {
-										player.kickPlayer("Spam!");
-										counts.remove(player);
-										continue;
-									}
-
-								} else {
-									counts.put(player, new CacheSpam(0.0D, null));
-								}
-							}
-						}
-
-						Bukkit.getPluginManager().callEvent(event);
-					}
-
-					logger.debug("chat.size: %s", chat.size());
-
-					Bukkit.getScheduler().runTaskLater(plugin, ChatLimiter::clear, 1L);
-				}
-
-				if (spam.getMax() > 0.0F) {
-					for (Map.Entry<Player, CacheSpam> entry : counts.entrySet()) {
-						Player player = entry.getKey();
-						CacheSpam count = entry.getValue();
-							count.setCount(count.getCount() + 1);
-						counts.put(player, count);
-					}
-
-					spam.setCount(spam.getCount() + 1);
-				}
+			private SpamTracker getGlobalSpamTracker() {
+				FileConfiguration config  = plugin.config.get();
+				return new SpamTracker(
+					0, // Contador de mensajes.
+					config.getInt("spam.max-messages"),
+					0,  // Contador de ticks.
+					config.getInt("spam.max-ticks")
+				);
 			}
 
-		}, 0L, 1L);
+			public void run() {
+				if (plugin.isDisabled()
+						|| spam.getTotalCount() == 0
+						|| spam.getTotalMax() == 0)
+					return;
+
+				if (spam.getActualMax() >= spam.getTotalMax()) {
+					counts.clear();
+
+					logger.debug("spam global: actualCount: %s, totalCount: %s, actualMax: %s, totalMax: %s",
+						spam.getActualCount(), spam.getTotalCount(), spam.getActualMax(), spam.getTotalMax());
+
+					spam = getGlobalSpamTracker();
+				}
+
+				logger.debug("chat.size: %s", chat.size());
+
+				for (int i = 0; !chat.isEmpty() && i < Math.min(spam.getTotalCount(), chat.size()); i++) {
+					Message event = chat.get(0);
+
+					if (event.getSender() instanceof Player) {
+						Player player = (Player) event.getSender();
+
+						if (!counts.containsKey(player))
+							counts.put(player, new SpamTracker(0, 0, 0, 0));
+
+						SpamTracker count = counts.get(player);
+						count.setActualCount(count.getActualCount() + 1);
+						counts.put(player, count);
+
+						logger.debug("count '%s': actualCount: %s, totalCount: %s, actualMax: %s, totalMax: %s",
+							player.getName(),
+							count.getActualCount(), count.getTotalCount(), count.getActualMax(), count.getTotalMax());
+
+						if (count.getActualCount() >= spam.getTotalCount() * 1.5) {
+							player.kickPlayer("SPAM!");
+							counts.remove(player);
+						}
+
+						if (count.getActualCount() >= spam.getTotalCount())
+							continue;
+					}
+
+					Bukkit.getPluginManager().callEvent(event);
+
+					try { remove(event); }
+					catch (Exception ignored) {}
+				}
+
+				// Aumentamos el contador de los ticks procesados.
+				spam.setActualMax(spam.getActualMax() + 5);
+			}
+
+		}, 0L, 5L);
 	}
 
 	public void stop() {
