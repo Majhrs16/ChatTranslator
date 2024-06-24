5a116f1fe8c43c74237267483ecf49bddf15b7e5
Majhrs16 <majhrs16.7@gmail.com>
2024-06-23 22:26:13 -0400

    Redirected debug logs to file "debug.log" in the plugin folder.

--- src/main/java/me/majhrs16/cht/events/LoggerListenerImpl.java
+++ src/main/java/me/majhrs16/cht/events/LoggerListenerImpl.java
@@ -1,10 +1,71 @@
 package me.majhrs16.cht.events;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import me.majhrs16.cht.util.cache.Config;
+import me.majhrs16.cht.ChatTranslator;
+
+import java.nio.file.FileSystems;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileWriter;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
 
 public class LoggerListenerImpl implements me.majhrs16.lib.logger.LoggerListener<String> {
+	private BufferedWriter debugFile;
+	private BukkitTask task;
+	private final ConcurrentLinkedQueue<String> debugQueue = new ConcurrentLinkedQueue<>();
+
+	public void start() {
+		if (!Config.DEBUG.IF())
+			return;
+
+		ChatTranslator plugin = ChatTranslator.getInstance();
+		String logFilePath = plugin.getDataFolder() + FileSystems.getDefault().getSeparator() + "debug.log";
+
+		try {
+			debugFile = new BufferedWriter(new FileWriter(logFilePath, false));
+
+		} catch (IOException e) {
+			error(e.toString());
+			return;
+		}
+
+		task = Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
+			String log = debugQueue.poll();
+			if (log == null) return;
+
+			try {
+				debugFile.write(log);
+				debugFile.newLine();
+				debugFile.flush();
+
+			} catch (IOException e) {
+				error(e.toString());
+			}
+
+		}, 0L, 5L);
+	}
+
+	public void stop() {
+		if (!Config.DEBUG.IF())
+			return;
+
+		if (task != null)
+			task.cancel();
+
+		try {
+			if (debugFile != null)
+				debugFile.close();
+
+		} catch (IOException e) {
+			error(e.toString());
+		}
+	}
+
 	private String format(String format, Object... args) {
 		StackTraceElement[] stackTrace = new Throwable().getStackTrace();
 		return stackTrace[6].getClassName() + "." + stackTrace[6].getMethodName() + ": " + String.format(format, args);
@@ -15,7 +76,7 @@ public class LoggerListenerImpl implements me.majhrs16.lib.logger.LoggerListener
 			return null;
 
 		String result = format(format, args);
-		Bukkit.getLogger().warning(result);
+		debugQueue.add(result);
 		return result;
 	}
 
